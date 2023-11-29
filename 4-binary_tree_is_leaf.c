b9e44facbcca6736baefbdf9cb2f8cbb7938242d
RadouaneBaba <radoine.baba04@gmail.com>
2023-11-29 12:06:55 -0800

    is root is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check if node is leaf
+ * @node: node to check
+ * Return: 1 if leaf otherwise 0
+ */
+
+int binary_tree_is_leaft(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left || node->right)
+		return (0);
+
+	return (1);
+}
